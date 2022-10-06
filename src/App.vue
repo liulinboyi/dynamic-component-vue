511a298e570d0060c0416e8deb3718edcfbf452c
liulinboyi <814921718@qq.com>
2022-10-06 22:49:07 +0800

    feat: add change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import { ref } from 'vue'
 import MyIcon from "./my-icon.vue";
-
+const name = ref("Icon2");
 </script>
 
 <template>
-  <MyIcon iconComponentName="Icon2" />
+  <button @click="name === 'Icon2' ? (name = 'Icon1') : (name = 'Icon2')">
+    change name
+  </button>
+  <MyIcon :iconComponentName="name" />
 </template>
