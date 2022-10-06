d9343f2d1b7b9bda341535e86ab399b9b3265bd2
liulinboyi <814921718@qq.com>
2022-10-06 22:15:14 +0800

    feat: update

--- src/my-icon.vue
+++ src/my-icon.vue
@@ -1,5 +1,23 @@
-<script>
+<script setup>
+import { computed } from "vue";
 import icons from "./icon.js";
+const props = defineProps({
+  iconComponentName: {
+    type: String,
+    default: "Icon1",
+  },
+});
+
+const components = icons;
+const name = computed(() => {
+  console.log(components, "components");
+  const result = components[props.iconComponentName];
+  console.log(result);
+  return result;
+});
+</script>
+
+<!-- <script>
 export default {
   name: "my-icon",
   components: { ...icons },
@@ -10,8 +28,8 @@ export default {
     },
   },
 };
-</script>
+</script> -->
 
 <template>
-  <component :is="iconComponentName" />
+  <component :is="name" />
 </template>
